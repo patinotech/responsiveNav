449635b9019d92820ce32d6458ae0ee4dc3072ef
InfamousFlame <owenlevin@gmail.com>
2016-09-06 22:49:14 -0700

    started working on website

---
+++ js/contact.js
@@ -0,0 +1,8 @@
+
+    
+    (function(d, t){
+    var g = d.createElement(t),
+          s = d.getElementsByTagName(t)[0];
+       g.src = "http://www.foxyform.com/js.php?id=773259&sec_hash=9a2a537e47b&width=350px";
+       s.parentNode.insertBefore(g, s);
+}(document, "script"));
